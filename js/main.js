7b105676161b69347058f3999af3077a4e06d255
hlaluphi <hlaluphi@duck.com>
2026-02-07 20:16:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ $('#yes').click(function() {
             }).then((result) => {
                 if (result.value) {
                     // Open phone dialer
-                    window.location = 'tel:0710722969';
+                    window.location = 'tel:0693091872';
                     
                     // Redirect to a page that closes itself
                     setTimeout(() => {
